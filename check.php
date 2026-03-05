ae1afd0627145c75c2b5678671e8c3ba98fa1f60
Buom537 <buom2023@gmail.com>
2026-03-05 09:26:47 +0200

    Add files via upload

---
+++ check.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Name PHP</title>
+</head>
+<body>
+    <h1>Your buom kuol  changath PHP</h1>
+
+    <pre>
+        Y
+        Y
+        Y
+        Y
+        YYYYY
+        Y
+        Y
+    </pre>
+
+    <p>SHA256 of my name is buom:</p>
+    <p>
+        <?php
+            echo hash('sha256', 'buom');
+        ?>
+    </p>
+</body>
+</html>
